371460707e09e965a63dae6249683a8fe2aaf6e6
Marcel Dechant <marcel.dechant@freenet.de>
2025-01-14 16:23:00 +0100

    account changed to auccount_app

--- videoflix/settings.py
+++ videoflix/settings.py
@@ -37,9 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts',
     'rest_framework',
-    'django.contrib.auth',
+    'accounts_app', 
 ]
 
 MIDDLEWARE = [
